9a387a9e9f3815e085b264ffa1632f46533c254a
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-04-08 11:55:16 +0200

    carousel done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../css/Card.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 function Card(props) {
   const { title, price, picture } = props.location.query.vestiaire.vetement;
@@ -7,13 +9,16 @@ function Card(props) {
   return (
     <div className="card">
       <div className="card__picture">
-        {picture.map((image) => {
-          return <img src={image} alt={title} />;
-        })}
+        <Carousel>
+          {picture.map((image) => {
+            return <img src={image} alt={title} />;
+          })}
+        </Carousel>
       </div>
       <div className="card__description">
         <h2>{title}</h2>
         <h3>{price}</h3>
+        <button className="card__btn">Ajouter au panier</button>
       </div>
     </div>
   );
